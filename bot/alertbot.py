76fde8c5cd46c3281e178df4d33af42b5ae82880
David Garcia <david.garcianunez@telefonica.com>
2021-11-23 17:18:34 +0100

    format code

--- bot/alertbot.py
+++ bot/alertbot.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-# pylint: disable=C0116,W0613
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-First, a few callback functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Example of a bot-user conversation using ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import sys
 import os
 
